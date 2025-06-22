9b7bad5c88d35dd4777637df1ee3c1c27b313deb
Anuraj Jit Saikia <anurajjit@gmail.com>
2025-06-22 10:06:52 +0530

    Ecofinds improved v5 added new page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from database import db
 from config import setup_database
 from models.product import Product
@@ -18,7 +18,7 @@ def root():
 
 #Index route
 @app.route("/products")
-def allProducts():
+def allListings():
     # Query all products
     products = Product.query.all()
     return render_template("index.html", products=products)
@@ -29,6 +29,29 @@ def showListing(id):
     product = Product.query.get_or_404(id)
     return render_template("show.html", product=product)
 
+#New route
+@app.route("/products/new")
+def renderNewForm():
+    return render_template("new.html")
+
+#Create Route
+@app.route("/products", methods=["POST", "GET"])
+def createListing():
+    if request.method == "POST":
+        # Create product directly from form data, with conversion for price
+        form_data = request.form.to_dict()
+        form_data['price'] = int(form_data['price'])
+        
+        new_product = Product(**form_data)
+        
+        # Add to database and commit
+        db.session.add(new_product)
+        db.session.commit()
+        print("Data Saved")
+        
+        # Redirect to the product listing page
+        return redirect(url_for('allListings'))
+    
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
--- models/product.py
+++ models/product.py
@@ -3,12 +3,14 @@ import uuid
 from sqlalchemy import String, Text, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
+DEFAULT_IMAGE = "https://images.unsplash.com/photo-1615397349754-cfa2066a298e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
 class Product(db.Model):
     __tablename__ = 'product'
     id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
-    image_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(255), default=DEFAULT_IMAGE)
     price: Mapped[int] = mapped_column(Integer, nullable=False)
 
     def __repr__(self) -> str:
